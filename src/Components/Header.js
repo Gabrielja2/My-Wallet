fdf24a08af8427d50f9f5533516cc077a61821ae
Gabriel <gabrielja2antunes@gmail.com>
2022-06-23 19:45:12 -0300

    componente header externo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,43 +1,29 @@
-/* import React, { Component } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import './Header.css';
-import requestAPI from '../Services/requestAPI';
 
 class Header extends Component {
-  state = {
-    initialValue: 0,
-    currencies: [],
-  }
-
-  componentDidMount =() => {
-    const { currencies } = this.state;
-    const retornoApi = requestAPI();
-    this.setState({
-      currencies: retornoApi,
-    });
-    console.log(currencies);
-  }
-
   render() {
-    const { initialValue } = this.state;
     const { email } = this.props;
     return (
       <header className="container-header">
         <div className="logo">
           {}
         </div>
-        <input
+        <p
+          readOnly
           data-testid="email-field"
-          value={ email }
-          type="email"
-        />
-        <input
+        >
+          { email }
+        </p>
+        <p
+          readOnly
           data-testid="total-field"
-          value={ `despesas total:${initialValue} ` }
-          type="email"
-        />
+        >
+          { `despesas total:${0} ` }
+        </p>
         <div
+          readOnly
           className="div-currency"
           data-testid="header-currency-field"
         >
@@ -56,4 +42,4 @@ const mapStateToProps = (state) => ({
   email: state.user.email,
 });
 
-export default connect(mapStateToProps)(Header); */
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import './Wallet.css';
 import { requestAPI } from '../actions';
+import Header from '../Components/Header';
 
 class Wallet extends Component {
   componentDidMount = async () => {
@@ -11,46 +12,22 @@ class Wallet extends Component {
   }
 
   render() {
-    const { email } = this.props;
+    // const { email } = this.props;
     return (
-      <header className="container-header">
-        <div className="logo">
-          {}
-        </div>
-        <p
-          readOnly
-          data-testid="email-field"
-        >
-          { email }
-        </p>
-        <p
-          readOnly
-          data-testid="total-field"
-        >
-          { `despesas total:${0} ` }
-        </p>
-        <div
-          readOnly
-          className="div-currency"
-          data-testid="header-currency-field"
-        >
-          BRL
-        </div>
-      </header>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
 
 Wallet.propTypes = {
   thunkFetch: PropTypes.func.isRequired,
-  email: PropTypes.string.isRequired,
+  // email: PropTypes.string.isRequired,
 };
 
-const mapStateToProps = (state) => ({
-  email: state.user.email,
-});
 const mapDispatchToProps = (dispatch) => ({
   thunkFetch: () => dispatch(requestAPI()),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
+export default connect(null, mapDispatchToProps)(Wallet);
