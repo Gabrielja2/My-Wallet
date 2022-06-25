228cc761d344695302fca3ab16cb46b056c647a4
Gabriel <gabrielja2antunes@gmail.com>
2022-06-25 10:30:29 -0300

    corrigindo erro na DOM

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -6,17 +6,19 @@ class Table extends Component {
     return (
       <div className="div-table">
         <table className="table">
-          <tr className="table-head">
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
+          <tbody>
+            <tr className="table-head">
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </tbody>
         </table>
       </div>
     );
